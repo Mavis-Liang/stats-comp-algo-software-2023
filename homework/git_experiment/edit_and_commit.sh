aeacc067b1e114d08fc354965f27c2e7b9a0a8e6
Mavis Liang <liangxw27@gmail.com>
2023-02-12 21:35:56 -0500

    Completed Exc1 and the answer to submit

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+module load tex
 for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
-  # Fill in
+  sed -i "$linenum i This is the first sentence." foo.tex
+  pdflatex foo.tex > /dev/null 2>&1
+  git add -u
+  git commit -m "Add $commit_count sentence to TeX file"
 done
